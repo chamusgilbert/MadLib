ea6ceea2fb13c0edd4df2d63376c32f4808b1aa7
famouschamus <chamusg@gmail.com>
2018-02-08 17:24:09 -0500

    added makyre

---
+++ MadLib/Makyre.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace MadLib
+{
+    class Makyre
+    {
+        public static void MakyreLibs()
+        {
+            string makyre = new StreamReader(@"C:\Users\Grand Circus Student\source\repos\MadLib\Makyre.txt").ReadToEnd();
+
+
+            Console.WriteLine(makyre, Constructor.GetAdjective(), Constructor.GetAdjective(), Constructor.GetAdjective(), Constructor.GetNoun(), Constructor.GetAdjective(), Constructor.GetVerb(), Constructor.GetVerb(), Constructor.GetAdjective(), Constructor.GetNumber(), Constructor.GetAdjective(), Constructor.GetPlace(), Constructor.GetPlace(), Constructor.GetVerb(), Constructor.GetVerb(), Constructor.GetVerb(), Constructor.GetVerb(), Constructor.GetAnimal(), Constructor.GetVerb());
+        }
+    }
+}
